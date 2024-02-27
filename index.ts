1a4e7d6c2d6e996958de152e0b31327cc6af2930
trog <admin@trog.host>
2024-02-27 15:28:10 -0800

    ^^

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import { $ } from "bun";
 
 Bun.serve({
   fetch(req: Request) {
-    const output = JSON.stringify(req);
-    console.log(output);
-    $`echo ${output} >> /tmp/DEVTERM_PRINTER_IN`;
+    console.log(req);
+    const targout = req.json();
+    $`echo ${targout} >> /tmp/DEVTERM_PRINTER_IN`;
     return new Response("ok");
   },
   port: 8005,
