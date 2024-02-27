797eb1cd104afc16456de610f2d2b0967a1a8aed
trog <admin@trog.host>
2024-02-27 15:08:55 -0800

    use Bun.write instead of $ to output to /tmp

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 Bun.serve({
   fetch(req: Request) {
-    Bun.$`echo ${req.body} > /tmp/DEVTERM_PRINTER_IN`;
+    Bun.write("/tmp/DEVTERM_PRINTER_IN", JSON.stringify(req.body));
     return new Response("ok");
   },
   port: 8005,
