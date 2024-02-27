03e66ea7df58ae6d69903f15b92d431d0f911d17
trog <admin@trog.host>
2024-02-27 15:05:18 -0800

    feat: serve http and echo req.body to printer input

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  fetch(req: Request) {
+    Bun.$`echo ${req.body} > /tmp/DEVTERM_PRINTER_IN`;
+    return new Response("ok");
+  },
+  port: 8005,
+});
