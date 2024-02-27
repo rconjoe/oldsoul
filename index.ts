8c0725ab6856df9923b914941461ebb7a3b84e0d
trog <admin@trog.host>
2024-02-27 15:13:24 -0800

    do the whole request

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 Bun.serve({
   fetch(req: Request) {
-    Bun.write("/tmp/DEVTERM_PRINTER_IN", JSON.stringify(req.body));
+    Bun.write("/tmp/DEVTERM_PRINTER_IN", JSON.stringify(req));
     return new Response("ok");
   },
   port: 8005,
