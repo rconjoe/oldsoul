3bd94917d98f8d910d21171596aac148923189db
trog <admin@trog.host>
2024-02-27 15:20:06 -0800

    try something weird with shell

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
+import { $ } from "bun";
+
 Bun.serve({
   fetch(req: Request) {
-    Bun.write("/tmp/DEVTERM_PRINTER_IN", JSON.stringify(req));
+    const output = JSON.stringify(req);
+    console.log(output);
+    $`echo ${output} >> /tmp/DEVTERM_PRINTER_IN`;
     return new Response("ok");
   },
   port: 8005,
