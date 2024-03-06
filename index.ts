510a1311c1aeb3385de45161fb3e15d313662660
trog <admin@trog.host>
2024-03-06 01:55:59 -0800

    ^

--- index.ts
+++ index.ts
@@ -6,5 +6,5 @@ for await (const chunk of Bun.stdin.stream()) {
   const chunkText = Buffer.from(chunk).toString();
   console.log(`Chunk: ${chunkText}`);
   Bun.write("/tmp/DEVTERM_PRINTER_IN", chunkText);
-  await $`echo ${chunkText}`;
+  await $`figlet ${chunkText} | tee /tmp/DEVTERM_PRINTER_IN`;
 }
