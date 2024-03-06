4e285424650794b705dfa5eaa9b2aa88c5686dd7
trog <admin@trog.host>
2024-03-06 02:38:34 -0800

    hotfixes

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { $ } from "bun";
 
+console.log("Enter text:")
+
 for await (const chunk of Bun.stdin.stream()) {
   // chunk is Uint8Array
   // this converts it to text (assumes ASCII encoding)
   const chunkText = Buffer.from(chunk).toString();
   console.log(`Chunk: ${chunkText}`);
-  Bun.write("/tmp/DEVTERM_PRINTER_IN", chunkText);
-  await $`figlet ${chunkText} | tee /tmp/DEVTERM_PRINTER_IN`;
+  // Bun.write("/tmp/DEVTERM_PRINTER_IN", chunkText);
+  await $`figlet -w 48 ${chunkText} | tee /tmp/DEVTERM_PRINTER_IN`;
 }
