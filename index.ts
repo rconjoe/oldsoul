02085c3c8fbcc61fc2c831ef597c2042f38cedf2
trog <admin@trog.host>
2024-03-06 11:16:45 -0800

    wip

--- index.ts
+++ index.ts
@@ -13,10 +13,11 @@ import { $ } from "bun";
 
 const server = Bun.serve({
   port: 4250,
-  fetch(req) {
+  async fetch(req) {
     console.log(req);
-    // await $`figlet -f banner -w 48 -Wk -c ${category} | tee /tmp/DEVTERM_PRINTER_IN`;
-    Bun.write("/tmp/DEVTERM_PRINTER_IN", req.url);
+    await $`figlet -f banner -w 48 -Wk -c ${req.headers["category"]} | tee /tmp/DEVTERM_PRINTER_IN`;
+    await $`figlet -f small -w 48 -Wk -l ${req.url} | tee /tmp/DEVTERM_PRINTER_IN`;
+    await $`echo ${req.headers["message"]} | tee /tmp/DEVTERM_PRINTER_IN`;
     return new Response("ok");
   },
 });
